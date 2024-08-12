9c4654c5b5e81be0aac4b9c5c022a25ef3bc59ea
James <jamespl2009@hotmail.com>
2024-08-12 18:05:56 -0500

    update

--- Geant4_Project/sim.cc
+++ Geant4_Project/sim.cc
@@ -1,61 +1,117 @@
-#include <iostream>
-
+// Geant4 Libraries
 #include "G4RunManager.hh"
 #include "G4MTRunManager.hh"
-#include "G4UImanager.hh"
-#include "G4VisManager.hh"
 #include "G4VisExecutive.hh"
 #include "G4UIExecutive.hh"
+#include "G4UImanager.hh"
+#include "G4VisManager.hh"
 
+// C++ Libraries
+#include "Randomize.hh"
+#include <iostream>
+
+namespace {
+  void PrintUsage() {
+    G4cerr << " Usage: " << G4endl;
+    G4cerr << " OpNovic [-m macro ] [-u UIsession] [-t nThreads] [-r seed] "
+           << G4endl;
+    G4cerr << "   note: -t option is available only for multi-threaded mode."
+           << G4endl;
+  }
+}
 
 int main(int argc, char** argv) {
     
-    //Create the user interface
-    /*Assigning a pointer to 0 is equivalent to assigning it to nullptr, it means
-    that ui does not point to a G4UIExecutive object*/
-    G4UIExecutive *ui = 0;
+    // Initial debug message
+    G4cout << "Starting Geant4 program..." << G4endl;
 
-    //Create the G4RunManager object
+    // Evaluate arguments
+    if ( argc > 9 ) {
+      PrintUsage();
+      return 1;
+    }
+
+    G4String macro;
+    G4String session;
     #ifdef G4MULTITHREADED
-       G4MTRunManager *runManager = new G4MTRunManager(); 
+      G4int nThreads = 0;
+      G4cout << "Multithreading support enabled." << G4endl;
     #else
-      G4RunManager *runManager = new G4RunManager(); 
+      G4cout << "Multithreading support NOT enabled." << G4endl;
     #endif
 
-    //Initialize the runManager
+    G4long myseed = time(NULL);
+
+    for ( G4int i=1; i<argc; i=i+2 ){
+      if      ( G4String(argv[i]) == "-m" ) macro   = argv[i+1];
+      else if ( G4String(argv[i]) == "-u" ) session = argv[i+1]; 
+      else if ( G4String(argv[i]) == "-r" ) myseed  = atoi(argv[i+1]);
+    #ifdef G4MULTITHREADED
+      else if ( G4String(argv[i]) == "-t" ){
+          nThreads = G4UIcommand::ConvertToInt(argv[i+1]);
+          G4cout << "Number of threads set to: " << nThreads << G4endl;
+      }
+    #endif
+      else {
+          PrintUsage();
+          return 1;
+      }
+    }
+
+    // Choose the Random engine
+    G4Random::setTheEngine(new CLHEP::RanecuEngine);
+    G4cout << "Random engine configured." << G4endl;
+
+    // Create the G4RunManager object with multithreading support if enabled
+    #ifdef G4MULTITHREADED
+      G4MTRunManager *runManager = new G4MTRunManager();
+      G4cout << "G4MTRunManager created (multithreading support)." << G4endl;
+      if ( nThreads > 0 ) runManager->SetNumberOfThreads(nThreads); 
+    #else
+      G4RunManager *runManager = new G4RunManager();
+      G4cout << "G4RunManager created (no multithreading support)." << G4endl;
+    #endif
+
+    // Seed the random number generator manually
+    G4Random::setTheSeed(myseed);
+    G4cout << "Seed set to: " << myseed << G4endl;
+
+    G4String physName = "QGSP_BERT_HP";
+
+    // Set mandatory initialization classes
+    
+
+    // Initialize G4 kernel
     //runManager->Initialize();
 
-    //if conditions
-    
-    if(argc == 1){
-    
-         ui = new G4UIExecutive(argc, argv);
-    
-    }
-    
-    
-    //Visualization manager
+    // Visualization manager
     G4VisManager *visManager = new G4VisExecutive();
     visManager->Initialize();
-    
-    G4UImanager *UImanager = G4UImanager::GetUIpointer();
-    
-    /*if(ui) condition means that ui is different from 0*/
-    if(ui){
-        
-          //UImanager->ApplyCommand("/control/execute vis.mac");
 
-          ui->SessionStart();
-          // Start the UI session, allowing interactive commands to be processed
+    // Get the pointer to the User Interface manager
+    G4UImanager *UImanager = G4UImanager::GetUIpointer();
+
+    if ( macro.size() ) {
+      // Batch mode
+      G4String command = "/control/execute ";
+      UImanager->ApplyCommand(command+macro);
+      //UImanager->ApplyCommand("/vis/open OGL");
+    } else { 
+      // Define UI session for interactive mode
+      G4UIExecutive * ui = new G4UIExecutive(argc, argv, session);
+      UImanager->ApplyCommand("/control/execute vis.mac");
+      //UImanager->ApplyCommand("/control/execute input.in");
+      if (ui->IsGUI()){
+        UImanager->ApplyCommand("/control/execute gui.mac");
+      } else {
+        G4cout << "IsGUI() returned false: NO GUI session detected." << G4endl;
+      }
+      ui->SessionStart();
+      delete ui;
     }
-    
-    else{
-          
-          G4String command = "/control/execute ";
-          G4String fileName = argv[1];
-          UImanager->ApplyCommand(command+fileName);
-    }
+
+    delete visManager;
+    delete runManager;
     
     return 0;
 }
-
