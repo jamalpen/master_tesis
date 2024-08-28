0dc08d3706ac0184cfb814c7b590d97b0122c054
JAMES <jamespl2009@hotmail.com>
2024-08-27 23:20:44 -0500

    Update

---
+++ G4_Container/G4_Project/build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,57 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/geant4lab/Geant4_projects/ActionInitialization.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/ActionInitialization.cc.o"
+  "/home/geant4lab/Geant4_projects/GeometryMessenger.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/GeometryMessenger.cc.o"
+  "/home/geant4lab/Geant4_projects/MyDetectorConstruction.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/MyDetectorConstruction.cc.o"
+  "/home/geant4lab/Geant4_projects/MyPhysicsList.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/MyPhysicsList.cc.o"
+  "/home/geant4lab/Geant4_projects/MySensitiveDetector.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/MySensitiveDetector.cc.o"
+  "/home/geant4lab/Geant4_projects/PrimaryGeneratorAction.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o"
+  "/home/geant4lab/Geant4_projects/PrimaryGeneratorMessenger.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/PrimaryGeneratorMessenger.cc.o"
+  "/home/geant4lab/Geant4_projects/PrimarySpectrum.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/PrimarySpectrum.cc.o"
+  "/home/geant4lab/Geant4_projects/PrimarySpectrumMessenger.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/PrimarySpectrumMessenger.cc.o"
+  "/home/geant4lab/Geant4_projects/RunAction.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/RunAction.cc.o"
+  "/home/geant4lab/Geant4_projects/sim.cc" "/home/geant4lab/Geant4_projects/build/CMakeFiles/sim.dir/sim.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/geant4lab/geant4/geant4-v10.7.3-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ G4_Container/G4_Project/build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/ActionInitialization.cc.o"
+  "CMakeFiles/sim.dir/GeometryMessenger.cc.o"
+  "CMakeFiles/sim.dir/MyDetectorConstruction.cc.o"
+  "CMakeFiles/sim.dir/MyPhysicsList.cc.o"
+  "CMakeFiles/sim.dir/MySensitiveDetector.cc.o"
+  "CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/sim.dir/PrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/sim.dir/PrimarySpectrum.cc.o"
+  "CMakeFiles/sim.dir/PrimarySpectrumMessenger.cc.o"
+  "CMakeFiles/sim.dir/RunAction.cc.o"
+  "CMakeFiles/sim.dir/sim.cc.o"
+  "sim"
+  "sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
